9799978a53afc9f0035bce5bf5c274bc988047d6
naut54 <edum77@icloud.com>
2024-02-27 20:14:39 +0100

    Loading Page addition

---
+++ web_snippets/loading_screen/assets/js/main.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function (){
+    var originalStyle = document.body.style.display;
+
+    document.body.style.display = 'none';
+
+    document.body.style.backgroundColor = "black";
+
+    // document.body.style.display = originalStyle;
+});
